e536563a2773cd721562ea4af6e0964916ea7abe
yuanconghao <yuanconghao@51talk.com>
2024-10-18 13:04:39 +0800

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from views import health_router
 from views import ocr_router, annotate_router
 
 app = FastAPI()
 
 # Include routers from views
+app.include_router(health_router, prefix="")
 app.include_router(ocr_router, prefix="/ocr", tags=["OCR"])
 app.include_router(annotate_router, prefix="/annotate", tags=["Annotate"])
--- views/__init__.py
+++ views/__init__.py
@@ -1,2 +1,3 @@
 from .ocr import router as ocr_router
 from .annotate import router as annotate_router
+from .health import router as health_router
