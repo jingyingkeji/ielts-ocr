bfa88faa08d15642d1d43eece05900f398950744
yuanconghao <yuanconghao@51talk.com>
2024-10-18 17:44:12 +0800

    add uploads api

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ app = FastAPI()
 
 # Include routers from views
 app.include_router(health_router, prefix="")
-app.include_router(ocr_router, prefix="/ocr", tags=["OCR"])
-app.include_router(annotate_router, prefix="/annotate", tags=["Annotate"])
-app.include_router(pdf_router, prefix="/pdf", tags=["PDF"])
-app.include_router(document_router, prefix="/document", tags=["Document"])
+app.include_router(ocr_router, prefix="/ielts/ocr", tags=["OCR"])
+app.include_router(annotate_router, prefix="/ielts/annotate", tags=["Annotate"])
+app.include_router(pdf_router, prefix="/ielts/pdf", tags=["PDF"])
+app.include_router(document_router, prefix="/ielts/document", tags=["Document"])
 
-app.include_router(upload_router, prefix="/upload", tags=["Images"])
+app.include_router(upload_router, prefix="/ielts/upload", tags=["Images"])
 
-app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+app.mount("/ielts/uploads", StaticFiles(directory="uploads"), name="uploads")
