3b1f34902e495c2eb67965e65df769ec657b4b4a
yuanconghao <yuanconghao@51talk.com>
2024-10-18 13:57:12 +0800

    first commit

--- views/annotate.py
+++ views/annotate.py
@@ -4,7 +4,7 @@ from controllers.annotate_controller import process_annotation
 router = APIRouter()
 
 @router.post("/generate_image")
-async def annotate_image(file: UploadFile = File(...), annotations: dict = None):
+async def generate_image(file: UploadFile = File(...), annotations: dict = None):
     if annotations is None:
         raise HTTPException(status_code=400, detail="Annotations are required")
     return await process_annotation(file, annotations)
--- views/ocr.py
+++ views/ocr.py
@@ -4,5 +4,5 @@ from controllers.ocr_controller import process_ocr
 router = APIRouter()
 
 @router.post("/get_ocr_data")
-async def upload_and_ocr(file: UploadFile = File(...)):
+async def get_ocr_data(file: UploadFile = File(...)):
     return await process_ocr(file)
