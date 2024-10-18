9976d71fbd3d96e84c1b49d71c0f78af47e428b4
yuanconghao <yuanconghao@51talk.com>
2024-10-18 18:44:55 +0800

    add uploads api

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-uvicorn main:app --host 0.0.0.0 --port 9003 --reload
+#uvicorn main:app --host 0.0.0.0 --port 9003 --reload
+nohup uvicorn main:app --host 0.0.0.0 --port 9003 --reload 2>/tmp/ielts-ocr.log 1>&2 &
