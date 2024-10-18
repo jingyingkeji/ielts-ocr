8e2ff2ac4256aebea8a04ce0ab7f5dec12b63dc6
yuanconghao <yuanconghao@51talk.com>
2024-10-18 17:40:32 +0800

    add uploads api

--- views/uploads.py
+++ views/uploads.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
+
 from controllers.uploads_controller import save_uploaded_image
 
 router = APIRouter()
 
-@router.post("/image/")
+
+@router.post("/image")
 async def upload_image(file: UploadFile = File(...)):
     if file.content_type not in ['image/jpeg', 'image/jpeg', 'image/png', 'image/gif']:
         raise HTTPException(status_code=400, detail="Unsupported file type. Please upload an image file.")
